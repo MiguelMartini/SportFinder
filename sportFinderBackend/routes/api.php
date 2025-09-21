b9a92bf7641e8c4a738b0e2986e3e4055242468d
MiguelMartini <miguelkaulingm21@gmail.com>
2025-09-21 18:49:27 -0300

    Feat: CRUD usuario

--- sportFinderBackend/routes/api.php
+++ sportFinderBackend/routes/api.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Http\Controllers\UsuarioController;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/usuarios', [UsuarioController::class, 'index']);
 
+Route::post('/usuarios', [UsuarioController::class, 'store']);
+
+Route::get('/usuarios/{id}', [UsuarioController::class, 'show']);
+
+Route::patch('/usuarios/edit/{id}', [UsuarioController::class, 'update']);
+
+Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy']);
